51a66ca07b69ed06f9044e2295cdee38c35e4d1f
Or Brener <obrener@uoguelph.ca>
2023-04-07 05:00:45 -0400

    finished add/remove element

--- script.js
+++ script.js
@@ -32,10 +32,76 @@ $(document).ready(
                  }
                 } );
             }
-            
         )
+
         if($('#heading0').length){
             $('#hidden').css('display', 'none');
         }
+
+        $("#elementSubmit").click(
+            function()
+            {
+            const form = new FormData()
+            
+            const num = $("#inputElementNumber").val()
+            const code = $("#inputElementCode").val()
+            const name = $("#inputElementName").val()
+            const col1 = $("#inputColor1").val()
+            const col2 = $("#inputColor2").val()
+            const col3 = $("#inputColor3").val()
+            const radius = $("#inputRadius").val()
+
+            form.append("num", num)
+            form.append("code", code)
+            form.append("name", name)
+            form.append("col1", col1)
+            form.append("col2", col2)
+            form.append("col3", col3)
+            form.append("radius", radius)
+        
+            $.ajax( {
+                url: 'elementSubmit',
+                type: 'POST',
+                data: form,
+                contentType: false,
+                processData: false,
+
+                success: function() {
+                    alert("Success!!!");
+                    location.href = 'http://localhost:50102/addElements.html'
+                },
+                error: function() {
+                    alert("Failure");
+                 }
+                } );
+            }
+        )
+
+        $("#deleteSubmit").click(
+            function()
+            {
+            const form = new FormData()
+            
+            const elementCode = $("#deleteCodeInput option:selected").text()
+            
+            form.append("elementCode", elementCode)
+
+            $.ajax( {
+                url: 'deleteSubmit',
+                type: 'POST',
+                data: form,
+                contentType: false,
+                processData: false,
+
+                success: function() {
+                    alert("Success!!!");
+                    location.href = 'http://localhost:50102/removeElements.html'
+                },
+                error: function() {
+                    alert("Failure");
+                 }
+                } );
+            }
+        )
     } );
   
--- server.py
+++ server.py
@@ -34,7 +34,7 @@ class MyHandler( BaseHTTPRequestHandler ):
             self.send_header( "Content-type", "text/html" )
             
             html_string = ""
-            with open('empty.html', 'r') as f:
+            with open('emptyMol.html', 'r') as f:
                 html_string = f.read()
 
             name = db.conn.execute("""SELECT Name FROM Molecules;""" ).fetchall()
@@ -86,14 +86,97 @@ class MyHandler( BaseHTTPRequestHandler ):
             # send the contents
             self.wfile.write( bytes( page, "utf-8" ) )
         
+        elif (self.path == "/removeElements.html"):
+            self.send_response( 200 ); # OK
+            self.send_header( "Content-type", "text/html" )
+            
+            html_string = ""
+            with open('emptyElements.html', 'r') as fpR:
+                html_string = fpR.read()
+
+            elements = db.conn.execute("""SELECT * FROM Elements;""" ).fetchall()
+            isElements = False
+            codes=[]
+            for numElement in range(len(elements)):
+                isElements = True
+                num = elements[numElement][0]
+                code = elements[numElement][1]
+                name = elements[numElement][2]
+                col1 = elements[numElement][3]
+                col2 = elements[numElement][4]
+                col3 = elements[numElement][5]
+                radius = elements[numElement][6]
+                codes.append(code)
+
+    
+                # Find the line number of the </tbody> tag
+                tbody_line_number = None
+                for i, line in enumerate(html_string.splitlines()):
+                    if '<!-- end of elements -->' in line:
+                        tbody_line_number = i
+                        break
+
+                # Append a new row right before the </tbody> tag
+                if tbody_line_number is not None:
+                    new_row = \
+                '''          <tr>
+                            <th scope="row">%s</th>
+                            <td>%s</td>
+                            <td>%s</td>
+                            <td>%s</td>
+                            <td>%s</td>
+                            <td>%s</td>
+                            <td>%s</td>
+                        </tr>'''% (num,code,name,col1,col2,col3,radius)
+                    
+                    lines = html_string.splitlines()
+                    lines.insert(tbody_line_number, new_row)
+                    html_string = '\n'.join(lines)
+            
+            if isElements:
+                new_row = \
+                    ''' <br>
+                    <form>
+                    <div class="form-row align-items-center">
+                        <div class="col-auto my-1">
+                        <label class="mr-sm-2 sr-only" for="inlineFormCustomSelect">Preference</label>
+                        <select class="custom-select mr-sm-2" id="deleteCodeInput">
+                            <option selected>Choose Element...</option>'''
+                for numCode in range(len(codes)):
+                    print(numCode)
+                    new_row += '''<option value="%s"">%s</option>''' % (numCode, codes[numCode])
+                
+                new_row += \
+                        '''</select>
+                        </div>
+                        <div class="col-auto my-1">
+                        <button type="submit" class="btn btn-primary" id="deleteSubmit">Delete</button>
+                        </div>
+                    </div>
+                    </form> '''
+                lines = html_string.splitlines()
+                lines.insert(55, new_row)
+                html_string = '\n'.join(lines)
+
+            with open('removeElements.html', 'w') as fpW:
+                fpW.write(html_string)
+            fp = open("removeElements.html")
+            page = fp.read()
+            fp.close()
+
+            # create and send headers
+            self.send_header( "Content-length", len(page) )
+            self.end_headers()
+
+            # send the contents
+            self.wfile.write( bytes( page, "utf-8" ) )
+        
         elif self.path == '/' or self.path.endswith( '.html' ):
             self.send_response( 200 ); # OK
             self.send_header( "Content-type", "text/html" )
 
             if (self.path == '/'):
                 fp = open( "index.html" )
-
-           
             else:
                 fp = open( self.path[1:] )
             page = fp.read()
@@ -179,6 +262,51 @@ class MyHandler( BaseHTTPRequestHandler ):
             fileData = io.TextIOWrapper(bytesIO)
 
             db.add_molecule(molName, fileData)
+             
+            self.send_response( 200 ); # OK
+            self.end_headers()
+
+        elif self.path == "/elementSubmit":
+
+            form = cgi.FieldStorage(
+                fp = self.rfile,
+                headers = self.headers,
+                environ = {"REQUEST_METHOD":"POST"}
+            )
+
+            num = form.getvalue("num")
+            code = form.getvalue("code")
+            name = form.getvalue("name")
+            col1 = form.getvalue("col1")
+            col2 = form.getvalue("col2")
+            col3 = form.getvalue("col3")
+            radius = form.getvalue("radius")
+
+            db['Elements'] = ( num, code, name, col1, col2, col3, radius )
+
+            # print("Elements")
+            # print( db.conn.execute( "SELECT * FROM Elements;" ).fetchall() )
+
+             
+            self.send_response( 200 ); # OK
+            self.end_headers()
+
+        elif self.path == "/deleteSubmit":
+
+            form = cgi.FieldStorage(
+                fp = self.rfile,
+                headers = self.headers,
+                environ = {"REQUEST_METHOD":"POST"}
+            )
+
+            elementCode = form.getvalue("elementCode")
+            print(elementCode)
+
+            db.conn.execute( """DELETE FROM Elements WHERE ELEMENT_CODE = '%s';""" % (elementCode) )
+
+            print("Elements")
+            print( db.conn.execute( "SELECT * FROM Elements;" ).fetchall() )
+
              
             self.send_response( 200 ); # OK
             self.end_headers()
